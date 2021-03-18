a9a8e0297851fb9c01541b34b227243c3bc4b942
kiishia <kiishia@hotmail.com>
2021-03-18 11:00:16 +0800

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,33 @@
+<footer class="container-fluid">
+      
+        <div class="container">
+          <div class="row">
+            <div class="col-md-6 text-center footer-content">
+              <p>JOSH WHITKEN</p>
+              </div>
+              
+              <img src="images/twitter.jpg" alt="twitter">
+              <img src="images/interent.jpg" alt="interent">
+              
+            </div> 
+              
+        <div class="col-md-6 text-center footer-content">
+              <p>TERMS AND CONDITIONS</p>
+            <img src="images/linkedin.jpg" alt="linkedin">
+            <img src="images/facebook.jpg" alt="facebook">
+              </div>
+              
+              
+              
+              
+              
+            </div>
+
+      
+      
+      </footer>
+
+
+  </body>
+</html>
+<?php wp_footer(); ?>
